084ea029c12ff54aa96033e1c2be3a553a64e2cd
Lucas Müllner <lucas.muellner@ebcont.com>
2025-07-05 14:12:07 +0200

    implements sample application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {Component, DestroyRef} from '@angular/core';
 import {MenuModule} from 'primeng/menu';
+import {HelloworldService} from "./services/helloworld.service";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+import {FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
+import {debounceTime, switchMap} from "rxjs";
 
 @Component({
   selector: 'o11y-root',
   standalone: true,
-  imports: [
-      MenuModule,
-      RouterOutlet
-  ],
+    imports: [
+        MenuModule,
+        ReactiveFormsModule
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -16,4 +19,21 @@ export class AppComponent {
 
   title = 'O11y\'s Handbook';
 
+  formControl = new FormControl('', Validators.required);
+  greeting = '';
+
+  constructor(
+      private readonly helloworldService: HelloworldService,
+      private readonly destroyRef: DestroyRef
+  ) {
+
+      this.formControl.valueChanges.pipe(
+          debounceTime(500),
+          switchMap((value) => value
+                  ? this.helloworldService.getGreetings(value)
+                  : this.helloworldService.getGreetings()
+          ),
+          takeUntilDestroyed(this.destroyRef),
+      ).subscribe(res => this.greeting = res.text);
+  }
 }
--- src/app/services/helloworld.service.ts
+++ src/app/services/helloworld.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+
+export interface Greeting {
+  text: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelloworldService {
 
-  constructor() { }
+  readonly #apiBaseUrl = 'http://localhost:8080/api';
+
+  constructor(private readonly http: HttpClient) { }
+
+  getGreetings(name?: string): Observable<Greeting> {
+    return this.http.get<Greeting>(`${this.#apiBaseUrl}/hello-world/greetings`, {
+      headers: { 'user': 'o11y' },
+      ...(name && { params: { name } }) // add param name if it is present
+    });
+  }
 }
